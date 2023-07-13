af759a92329d099ae63f2a242c4f525d94d87aa5
SeungGukYoo <dbtmd878@gmail.com>
2023-07-13 16:59:23 +0900

    ui 경로 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}", // <-- Add this line
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},

2c8f2d797cbb61739e177114584225154ef1acaa
SeungGukYoo <dbtmd878@gmail.com>
2023-07-14 20:03:31 +0900

    url test

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,7 @@ interface IData {
 }
 
 const getPosts = async () => {
-  let base_url: string | undefined;
-  if (process.env.NODE_ENV === "development") {
-    base_url = process.env.NEXT_PUBLIC_API_LOCAL_URL;
-  } else {
-    base_url = process.env.NEXT_PUBLIC_VERCEL_URL;
-  }
-
+  let base_url = process.env.NEXT_PUBLIC_LOCAL_URL;
   const data: IData = await fetch(`${base_url}/api/getPosts`).then((result) => result.json());
   return data;
 };
